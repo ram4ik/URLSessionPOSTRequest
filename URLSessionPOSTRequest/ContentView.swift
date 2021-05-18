f76864b599326960ebd1f841555f7dcba8e4cf51
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-05-18 13:12:51 +0300

    Initial Commit

---
+++ URLSessionPOSTRequest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  URLSessionPOSTRequest
+//
+//  Created by ramil on 18.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ URLSessionPOSTRequest/URLSessionPOSTRequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionPOSTRequestApp.swift
+//  URLSessionPOSTRequest
+//
+//  Created by ramil on 18.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionPOSTRequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
